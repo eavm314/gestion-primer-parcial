98570571c451d00e7d423af88c7a7e3498cd6c8a
Khenya-dev <brenda.alvarado@assuresoft.com.bo>
2024-11-19 20:15:32 -0400

    Feat: test tools

--- src/pages/FaseControl.vue
+++ src/pages/FaseControl.vue
@@ -1,4 +1,15 @@
 <template>
+<h2 class="text-xl font-bold mx-8">Herramientas</h2>
+    <div class="flex flex-row justify-center">
+        <img
+          src="../assets/images/pruebas/Selenium.png"
+          class="my-4 w-80 h-40 object-contain"
+        />
+        <img
+          src="../assets/images/pruebas/zap.jpg"
+          class="my-4 w-80 h-40 object-contain"
+        />
+    </div>
 </template>
   
 <script lang="ts">
